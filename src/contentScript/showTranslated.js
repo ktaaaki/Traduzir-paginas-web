1abea79bd25b5081505cf0f66fae1e33b716fb5a
ktaaaki <kaneko.sadaha@gmail.com>
2023-03-05 13:04:56 +0900

    exclude math tags from translation

--- src/contentScript/showTranslated.js
+++ src/contentScript/showTranslated.js
@@ -90,7 +90,7 @@ Promise.all([twpConfig.onReady(), getTabHostName()]).then(function (_) {
     "var",
   ];
   const htmlTagsInlineIgnore = ["br", "code", "kbd", "wbr"]; // and input if type is submit or button, and <pre> depending on settings
-  const htmlTagsNoTranslate = ["title", "script", "style", "textarea", "svg"];
+  const htmlTagsNoTranslate = ["title", "script", "style", "textarea", "svg", "math"];
 
   if (twpConfig.get("translateTag_pre") !== "yes") {
     htmlTagsInlineIgnore.push("pre");
